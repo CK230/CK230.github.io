1a1692e6bda200bfff8fee7279d272bea67c2805
ck <3301559202@qq.com>
2025-07-04 16:43:55 +0800

    delete something unimportant

--- main.js
+++ main.js
@@ -67,8 +67,6 @@ function showArticle(articleId) {
 function loadArticleContent(articleId, targetElement) {
     // 根据文章ID确定对应的HTML文件名
     const fileMap = {
-        'blog': 'pages/blog.html',
-        'aicode': 'pages/aicode.html',
         'html' : 'pages/Html.html'
     };
     const fileName = fileMap[articleId];
